68a9044b34317107e77c4cd1b2879d00b9297b33
jesusantguerrero <me@jesusantguerrero.com>
2017-12-14 12:54:46 -0400

    :white_check_mark: add specs for profile, aducation, and networks

--- spec/specs/curriculum.spec.js
+++ spec/specs/curriculum.spec.js
@@ -1,11 +1,49 @@
 describe("CV Builder", () => {
+  const Curriculum = require('./../../lib/cvutils/Curriculum')
+  const Profile = require('./../../lib/cvutils/Profile')
+  const Education = require('./../../lib/cvutils/Education')
+  let curriculum, profile
+  
+  beforeEach(() => {
+    curriculum = new Curriculum()
+    profile = new Profile('Jesus Guerrero', 'Web Developer','Always learning developing and mastering', 'jesusant.guerrero@gmail.com', '829-844-1674')
+  })
 
   it('should set basic person data', () => {
-    const Curriculum = require('./../../lib/cvutils/Curriculum')
-    const curriculum = new Curriculum()
-    const profile = new profile()
-
     curriculum.setProfile(profile)
     expect(curriculum.getProfile()).toBe(profile)
+  })
+
+  it('should empty the profile', () => {
+    curriculum.emptyProfile()
+    expect(curriculum.getProfile()).toBe(null)
+  })
+
+  describe('Education', () => {
+    let educationItem 
+
+    beforeEach(() => {
+      educationItem =  new Education('Bachiller Tecnico', 'Lilian Bayona', 2011, 2013, 5.0)
+    })
+
+    it('should add a new education item', () => {
+      curriculum.addEducation(educationItem)
+      expect(curriculum.getEducation()[0]).toBe(educationItem)
+    })
+
+    it('should delete the education item by index', () => {
+      curriculum.deleteEducation(0)
+      expect(curriculum.getEducation().length).toBe(0)
+    })
+  })
+
+  describe('networks', () => {
+    it('should add a new network to the profile', () => {
+      const github = curriculum.addNetwork('github', 'gh', 'https://github.com/jesusantguerrero')
+      const twitter = curriculum.addNetwork('twitter', 't', 'https://twitter.com/jesusntguerrero')
+
+      expect(curriculum.getNetworks().toString()).toBe([github, twitter].toString())
+
+    })
   })
 })
